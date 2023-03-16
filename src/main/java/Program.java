7324d5596bbc1ddf8158dc352a09e0bb68e378ae
cml1010101 <115354809+cml1010101@users.noreply.github.com>
2023-03-16 15:24:25 -0400

    few changes based off of feedback

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -7,6 +7,8 @@ import javax.swing.JFrame;
 
 import edu.wpi.first.cscore.CameraServerJNI;
 import edu.wpi.first.math.WPIMathJNI;
+import edu.wpi.first.networktables.DoubleArrayPublisher;
+import edu.wpi.first.networktables.DoubleArraySubscriber;
 import edu.wpi.first.networktables.DoublePublisher;
 import edu.wpi.first.networktables.DoubleSubscriber;
 import edu.wpi.first.networktables.NetworkTable;
@@ -15,20 +17,22 @@ import edu.wpi.first.networktables.NetworkTablesJNI;
 import edu.wpi.first.util.CombinedRuntimeLoader;
 import edu.wpi.first.util.WPIUtilJNI;
 
-public class NTClient implements KeyListener
+public class Program implements KeyListener
 {
     private static final double FPS = 60;
     private DoubleSubscriber trow, tcol;
     private DoublePublisher wrow, wcol;
+    private DoubleArrayPublisher parray;
+    private DoubleArraySubscriber sarray;
     public static void main(String[] args) throws IOException
     {
         NetworkTablesJNI.Helper.setExtractOnStaticLoad(false);
         WPIUtilJNI.Helper.setExtractOnStaticLoad(false);
         WPIMathJNI.Helper.setExtractOnStaticLoad(false);
         CameraServerJNI.Helper.setExtractOnStaticLoad(false);
-        CombinedRuntimeLoader.loadLibraries(NTClient.class, "wpiutiljni", "wpimathjni", "ntcorejni",
+        CombinedRuntimeLoader.loadLibraries(Program.class, "wpiutiljni", "wpimathjni", "ntcorejni",
                 "cscorejnicvstatic");
-        new NTClient().run();
+        new Program().run();
     }
     private void run() {
         NetworkTableInstance inst = NetworkTableInstance.getDefault();
@@ -40,6 +44,8 @@ public class NTClient implements KeyListener
         tcol = table.getDoubleTopic("col").subscribe(0.0);
         wrow = table.getDoubleTopic("row").publish();
         wcol = table.getDoubleTopic("col").publish();
+        sarray = table.getDoubleArrayTopic("array").subscribe(new double[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
+        parray = table.getDoubleArrayTopic("array").publish();
         JFrame frame = new JFrame("Northern Force's Piece Display Client");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
@@ -58,10 +64,35 @@ public class NTClient implements KeyListener
         while (true)
         {
             long startTimeMillis = System.currentTimeMillis();
+            double[] arr = sarray.get();
             g.setColor(Color.WHITE);
             g.fillRect(0, 0, 900, 500);
             g.setColor(Color.GREEN);
             g.fillRect((int)(tcol.get() * 100), (int)(trow.get() * 500 / 3), 100, 500 / 3);
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (arr[j * 9 + i] == 1)
+                    {
+                        g.setColor(Color.BLUE);
+                        g.fillRect((int)(i * 100), (int)(j * 500 / 3), 100, 500 / 3);
+                    }
+                    if (j != 2)
+                    {
+                        if (i == 0 || i == 2 || i == 3 || i == 5 || i == 6 || i == 8)
+                        {
+                            g.setColor(Color.BLACK);
+                            g.fillRect(i * 100 + 40, (int)(j * 500.0 / 3) + 40, 20, (int)(500.0 / 3 - 40));
+                        }
+                        else
+                        {
+                            g.setColor(new Color(0xFF, 0, 0xFF));
+                            g.fillRect(i * 100 + 20, (int)(j * 500.0 / 3 + 53.33), 60, 60);
+                        }
+                    }
+                }
+            }
             long endTimeMillis = System.currentTimeMillis();
             long diff = endTimeMillis - startTimeMillis;
             if (diff < (1000.0 / FPS))
@@ -81,110 +112,22 @@ public class NTClient implements KeyListener
     public void keyPressed(KeyEvent arg0) {
         switch (arg0.getKeyChar())
         {
-        case '1':
-            wcol.set(0);
-            break;
-        case '2':
-            wcol.set(1);
-            break;
-        case '3':
-            wcol.set(2);
-            break;
-        case '4':
-            wcol.set(3);
-            break;
-        case '5':
-            wcol.set(4);
-            break;
-        case '6':
-            wcol.set(5);
-            break;
-        case '7':
-            wcol.set(6);
-            break;
-        case '8':
-            wcol.set(7);
-            break;
-        case '9':
-            wcol.set(8);
-            break;
-        case 'q':
-            wrow.set(0);
-            break;
         case 'w':
-            wrow.set(0);
-            break;
-        case 'e':
-            wrow.set(0);
-            break;
-        case 'r':
-            wrow.set(0);
-            break;
-        case 't':
-            wrow.set(0);
-            break;
-        case 'y':
-            wrow.set(0);
-            break;
-        case 'u':
-            wrow.set(0);
-            break;
-        case 'i':
-            wrow.set(0);
-            break;
-        case 'o':
-            wrow.set(0);
-            break;
-        case 'p':
-            wrow.set(0);
+            wrow.set(Math.max(trow.get() - 1, 0));
             break;
         case 'a':
-            wrow.set(1);
+            wcol.set(Math.max(tcol.get() - 1, 0));
             break;
         case 's':
-            wrow.set(1);
+            wrow.set(Math.min(trow.get() + 1, 2));
             break;
         case 'd':
-            wrow.set(1);
+            wcol.set(Math.min(tcol.get() + 1, 8));
             break;
-        case 'f':
-            wrow.set(1);
-            break;
-        case 'g':
-            wrow.set(1);
-            break;
-        case 'h':
-            wrow.set(1);
-            break;
-        case 'j':
-            wrow.set(1);
-            break;
-        case 'k':
-            wrow.set(1);
-            break;
-        case 'l':
-            wrow.set(1);
-            break;
-        case 'z':
-            wrow.set(2);
-            break;
-        case 'x':
-            wrow.set(2);
-            break;
-        case 'c':
-            wrow.set(2);
-            break;
-        case 'v':
-            wrow.set(2);
-            break;
-        case 'b':
-            wrow.set(2);
-            break;
-        case 'n':
-            wrow.set(2);
-            break;
-        case 'm':
-            wrow.set(2);
+        case 'q':
+            double[] arr = sarray.get();
+            arr[(int)(trow.get() * 9 + tcol.get())] = 1 - arr[(int)(trow.get() * 9 + tcol.get())];
+            parray.set(arr);
             break;
         }
     }
